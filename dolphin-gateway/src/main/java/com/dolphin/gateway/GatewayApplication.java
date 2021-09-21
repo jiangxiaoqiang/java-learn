434ff654dea9d0e34b32d44539434dde9bc4c25c
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-09-20 23:52:14 -0400

    [feature] tweak the filter

--- dolphin-gateway/src/main/java/com/dolphin/gateway/GatewayApplication.java
+++ dolphin-gateway/src/main/java/com/dolphin/gateway/GatewayApplication.java
@@ -1,10 +1,12 @@
 package com.dolphin.gateway;
 
+import com.dolphin.gateway.filter.RequestTimeGatewayFilterFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author dolphin
@@ -17,5 +19,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory elapsedGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
     }
 }
--- dolphin-gateway/src/main/java/com/dolphin/gateway/filter/JwtAuthenticationFilter.java
+++ dolphin-gateway/src/main/java/com/dolphin/gateway/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -21,15 +22,29 @@ import java.util.function.Predicate;
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
     @Value("${dolphin.gateway.jwt.verify:true}")
-    private boolean jwtVerfiy;
+    private boolean jwtVerify;
 
     final static List<String> apiEndpoints = List.of("/register", "/login");
-
     final static Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
             .noneMatch(uri -> r.getURI().getPath().contains(uri));
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if(!jwtVerify){
+            return chain.filter(exchange);
+        }
+        if(exchange.getRequest() instanceof ServletServerHttpRequest){
+            ServletServerHttpRequest request = (ServletServerHttpRequest) exchange.getRequest();
+            if (isApiSecured.test(request)) {
+                return authImpl(request,exchange,chain);
+            }
+        }else{
+            log.error("request not a ServletServerHttpRequest");
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> authImpl(ServletServerHttpRequest request,ServerWebExchange exchange,GatewayFilterChain chain){
         return chain.filter(exchange);
     }
 
